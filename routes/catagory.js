95da15044322146b8d281324e97dbc10c83bb747
wenjing100 <55542032+wenjing100@users.noreply.github.com>
2021-04-14 21:38:57 +0800

    目录接口

--- routes/catagory.js
+++ routes/catagory.js
@@ -3,6 +3,28 @@ var router = express.Router();
 const cors = require('cors');
 const { exec } = require('../db/mysql.js');
 
+router.get('/', cors(), function (req, res, next) {
+  function setSql({ id = 1, size = 20}) {
+    let sql = ``;
+    if(id == 1){
+      sql = `SELECT * FROM wenjing_01.catagory_show`
+    }else{
+      sql = `select iid,g_name,inventory,price,marketprice,top_imgs from wenjing_01.goods_details where cat = ${id} limit ${size}`
+    }
+    return sql
+  }
+  exec(setSql(req.query), [], function (result, fields) {
+    console.log('返回catagory--show');
+    if (result.length) {
+      res.json({
+        status: 0,
+        data: result,
+        msg: '查询数据成功'
+      })
+    }
+  })
+});
+
 router.get('/side', cors(), function (req, res, next) {
   let sql = `select * from category where pid between 2 and 4;`
   exec(sql, [], function (result, fields) {
@@ -16,6 +38,4 @@ router.get('/side', cors(), function (req, res, next) {
     }
   })
 });
-
-
 module.exports = router;
